40ae79c6812859a28a46fd4a4edbac9e3602d8b0
Max <0xmaxyz@gmail.com>
2025-10-31 13:38:57 +0330

    use dark theme for rainbow

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,28 +1,34 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './styles/globals.css'
-import '@rainbow-me/rainbowkit/styles.css'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { WagmiProvider } from 'wagmi'
-import { RainbowKitProvider } from '@rainbow-me/rainbowkit'
-import { config } from './wagmi'
-import App from './App'
-import { ToastProvider } from './components/common/ToastHost'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./styles/globals.css"
+import "@rainbow-me/rainbowkit/styles.css"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { WagmiProvider } from "wagmi"
+import { RainbowKitProvider, darkTheme } from "@rainbow-me/rainbowkit"
+import { config } from "./wagmi"
+import App from "./App"
+import { ToastProvider } from "./components/common/ToastHost"
 
 const client = new QueryClient()
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={client}>
-        <RainbowKitProvider>
-          <ToastProvider>
-            <App />
-          </ToastProvider>
-        </RainbowKitProvider>
-      </QueryClientProvider>
-    </WagmiProvider>
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <WagmiProvider config={config}>
+            <QueryClientProvider client={client}>
+                <RainbowKitProvider
+                    theme={darkTheme({
+                        accentColor: "#6366f1",
+                        accentColorForeground: "white",
+                        borderRadius: "medium",
+                        fontStack: "system",
+                        overlayBlur: "small",
+                    })}
+                >
+                    <ToastProvider>
+                        <App />
+                    </ToastProvider>
+                </RainbowKitProvider>
+            </QueryClientProvider>
+        </WagmiProvider>
+    </React.StrictMode>
 )
-
-
